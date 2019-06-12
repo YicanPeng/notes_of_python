09d2c30892efa24bfbc28a657a62ebb08253a5a2
YicanPeng <531973446@qq.com>
2019-06-12 22:21:34 +0800

    初步笔记

--- note-python001.py
+++ note-python001.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-#"""
-
+""" 除关键字外,python变量赋值就是声明,变量赋值为空,就是内容不定的变量 """
 # import keyword as k
 # print(k.kwlist)
+""" python对缩进敏感,冒号表示开始子句,缩进表示子句所处的层次 """
+# if True:
+#     print("yes")
+# else:
+#     print('no')
+""" python语句换行使用反斜杠\ """
+""" 反斜杠也是文本字符转义符号 \"""
+# 文本引号前加r表示raw,反转义,使转义符和文本标点符(如换行符)无效
+""" 三引号字符串所见即所得 """
+""" python空行只是为了区分函数块,无实际意义 """
+""" 同一行内多条语句时,python使用;分号表示语句结束(不推荐一行多条语句) """
+print("\n")
+print(r"\n")
+print(""" \n """)
