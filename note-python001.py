253d2f9d6e267140a1ecd9c881f638860e236560
YicanPeng <531973446@qq.com>
2019-06-20 22:48:17 +0800

    添加函数,数据结构笔记

--- note-python001.py
+++ note-python001.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """ 程序 = 数据结构 + 算法,数据结构分3种:排序,判断(选择),循环 """
 """ 递归算法本质是数学归纳法,函数在内部调用自身,即计算第N项与第N-1项的关系,而后一直倒推到第1项,也就是基例
-   运用的数据结构其实是栈stack,先堆栈push后弹出pop """
+   运用的数据结构其实是栈stack,先堆栈push(放进第一项)后弹出pop(移除最后一项) """
 """ 1.基础语法,注释 """
 """ 除关键字外,python变量赋值就是声明变量,字典列表集合赋值为空,就是元素不定的变量 """
 """ 交叉赋值,同时赋值是python特色,使得本来需要中间过渡变量的交叉赋值,同时完成 """
@@ -39,17 +39,18 @@ str000="runoob"
    索引可以连续索引,如下例tup001[1][0] """
 """ 列表和元组是迭代器,用于循环遍历 """
 """ 字符串,列表和数组可以同步成对赋值多个变量,但是变量的数量必须与列表和元组的元素数量相等 """
-""" 列表的追加.append,插入.insert,删除.remove,移除.pop"""
+""" 列表的追加.append,插入.insert,删除.remove,移除.pop(默认最后一项)"""
 num001=1234;str001="runoob";tup001=(num001,str001,0)
 """推导式语句: 自变量表达式 for 自变量1 in 范围1 if 条件句1 自变量2 in 范围2 if 条件句2 
-   推导式语句中 for...in...if... 相当于定语(状语)从句,因此不能加逗号隔开"""
+   推导式语句中 for...in...if... 相当于定语(状语)从句,因此不能加逗号隔开;
+   python中逗号仅用于同步成对元素的同时赋值,详见zip函数和enumerate函数"""
 """ range函数的头标是0,左闭右开区间 """
-lis_001=[x+y+z for z in range(2) for x in range(2) for y in range(2) ]
+# lis001_1=[x+y+z for z in range(1) for x in range(2) for y in range(3) ]
 # num001[0]=5
 # str001[0]="x"
 # tup001[0]="a"
 # print(tup001[1][0])
-# print(lis_001)
+# print(lis001_1) 
 # print(len(lis_001))
 num002_0,num002_1,num002_2,num002_3,num002_4,num002_5="abcdefg"[0:-1]
 # print((num002_0)*3)
@@ -116,9 +117,11 @@ tup004_5_3=(1,2,3)
 zip004_5_1=zip(lis004_5_1,lis004_5_2)
 zip004_5_2=zip(lis004_5_1,lis004_5_2,tup004_5_3) #不匹配
 dic004_5_1=dict(zip004_5_1)
-""" zip对象无法展开 """
 # print(list(zip004_5_1))
 # print(dic004_5_1)
+""" zip对象可以通过迭代展开 """
+# for x,y,z in zip004_5_2:
+#    print(x,y,z,sep="@",end="\n")
 """ zip函数用于把多个列表配对成成对元组作为元素的一维列表,其反用法如下 """
 lis004_a,lis004_b =zip(*zip(lis004_5_2,tup004_5_3))
 # print(lis004_a,lis004_b)
@@ -141,7 +144,7 @@ num005_3 = 1
 # else:
 #    print("\n",num005_3,"大于或等于10")
 """ for...in...else...循环是有限循环,用于遍历序列(iter)
-   for循环中,else只在全部遍历之后才执行;循环break则不会执行 """
+   for循环中,else只在全部遍历之后才执行;循环异常中断break则不会执行 """
 """ break终止循环 """
 # for str005_1 in "asdfgh":
 #    if str005_1 == "f":
@@ -167,27 +170,17 @@ ite006_1=iter(lis006_1)
 """ 自定义类中使用__iter__()和__next__()内置函数构造迭代器属性,详见自定义类 """
 """ 自定义函数中使用yield关键字,可通过内置循环条件生成可迭代的yield对象,且不占用内存,详见自定义类
    这类自定义函数被称为生成器generator """
-""" for循环中使用迭代器,会自动调用next函数 """
-
-
-
-
-
-
-
-
-
-
-
+""" for...in 迭代变量: 循环语句中使用有迭代属性的变量,会自动调用next函数,类似使用序列 """
 
 """ 内置函数和自定义函数 """
 """ 不可变对象(数字,字符串,元组)相当于VBA的值传递,原有变量不会被函数运算重新赋值;
    可变对象类似引用传递,与函数共享同一内存指针,会在函数运算后被重新赋值 """
 """ 函数没有return,返回值是None """
-""" 自定义函数时,可以声明缺省参数,如本例中的l参数和n参数
-   加*的参数是不定长参数,类似VBA的可选参数,加一个*,返回值是元组类型,如本例m参数;
-      加两个*,返回值是字典类型
+""" 自定义函数时,可以声明缺省参数,如本例中的l参数和n参数,如有非默认参数,则默认参数必须放在函数定义语句的最后
+   加*的参数是不定长参数,类似VBA的可选参数,加一个*,参数以元组类型代入,如本例m参数;
+      加两个*,参数以字典类型代入
    在引用函数时,不定长参数之后的参数必须使用索引名赋值,如本例n参数 """
+""" 自定义函数最好以return结尾,而且python中return可以返回多个值 """
 def fibo(l=100,*m,n=100):
    a,b=0,1
    list_fibo=[]
@@ -199,12 +192,15 @@ def fibo(l=100,*m,n=100):
    return list_fibo
 """ 交叉赋值,同时赋值是python特色之一 """
 """ 本例中第3参数n必须使用索引名赋值 """
-print(fibo(100,n=1))
-""" map函数两个参数,第一个是内嵌函数,后面是内嵌函数所有参数的序列,map函数会遍历序列,分别代入
-   (内嵌函数参数不能空,但可以是None,以最短参数序列为准),
-   并按第一个原序列的数据类型和返回值生成一个map对象(不是序列,需要使用格式转换函数变成序列) """
-# print(tuple(map(fibo,[100,200,300],(None,),(None,None))))
-# print(tuple(map(fibo,[100,200,300],(None,None),(None,None)))[0][0])
+# print(fibo(100,n=1))
+""" map函数两个参数,第一个是内嵌函数,后面是内嵌函数所有参数的迭代器,map函数会遍历迭代器,分别代入
+   (函数参数不能空,但可以是None,以最短参数序列为准),
+   并按第一个原序列的数据类型和返回值生成一个map对象(不是序列,需要使用格式转换函数如tuple,list变成序列) """
+# map008=map(fibo,[100,200,300],(None,1),(None,None))
+# tup008=tuple(map008)
+# ite008=iter(tup008)
+# print(next(ite008),next(ite008))
+# print(tup008[0][0:-3:-1])
 """ 模块 """
 """ 只有包含__init__.py模块的文件夹才会被python认定为包,以免识别其他文件 """
 """ 包和模块有两种引入方式,即:import 模块 和 from 模块 import 函数 ;
@@ -222,11 +218,48 @@ fb = fibo
 # fb()
 """ 文件引用自身内置属性和方法,无需前缀,__name__属性永远是__main__ """
 # print(__name__)
+""" enumerate函数可以把列表或元组的索引序数和元素成对对应成类似字典键:值的关系的元组,默认从0开始索引 """
+str008_1="asdfgh"
+enu008_1=enumerate(str008_1)
+dic008_1={i:ele for i,ele in enu008_1}
+# for k,v in enumerate("asdf"):
+#    print(k)
+# for k in enumerate(list("asdf")):
+#    print(k)
+""" 无法遍历提前生成的enumerate对象,只能遍历语句中的enumerate函数结果 """
+# for k,v in enu008_1:
+#    print(k)
+# for k,v in dic008_1.items():
+#    print(k,v)
+""" lambda 变量1,变量2,变量3... : 表达式 语句用于创建简单的匿名函数,仅可以使用自身参数变量 """
+sm = lambda x,y,z : x+y+z
+# print(sm(1,2,3))
+""" Python 中只有模块（module），类（class）以及函数（def、lambda）才会引入新的作用域，
+   其它的代码块（如 if/elif/else/、try/except、for/while等）是不会引入新的作用域的，
+   也就是说这些语句内定义的变量，外部也可以访问 """
+""" python按照LEGB的顺序查找变量,Local局域作用域,Enclosing内嵌函数的外包函数的作用域,Global全局作用域,Builtin内置作用域(内置函数所在模块) """
+# if True:
+#    msg008="welcome"
+# print(msg008)
+""" nonlocal关键字可以使内嵌引用外包函数的变量,而且是引用传递,会改变该变量的值 """
+""" global关键字可以使函数直接引用外部的全局变量 """
+glo008_1=1
+def out008():
+   global glo008_1
+   num = glo008_1
+   def in008():
+      nonlocal num
+      num=num+2
+   print(num*2,in008(),num*2)
+   return
+# out008()
 
-
-
-
-
+""" 数据结构 """
+""" 列表可以使用.append方法和.pop方法使其成为堆栈stack(后进先出) """
+""" 列表可以使用.append方法和.popleft方法使其成为队列queue(先进先出) """
+""" 列表可以使用.clear方法清空,也可以使用del 列表[:] 删除全索引的方式清空 """
+""" reversed函数倒转序列 """
+""" sorted函数生成排序的序列 """
 
 
 
@@ -289,11 +322,12 @@ class iteration001:
       self.y = y
       self.x = x
       self.a = 0
-   """ 除init内置函数外,其他函数必须有return语句,且如果无返回值,则返回self """  
+   """ 即使无返回值,函数最好以return语句结尾;自定义类中,如果无返回值,只是定义私有属性,则return self """  
    def __iter__(self):
       return self
    """ 本例中使用条件语句触发raise stopiteration终止迭代 """
    """ python3中,迭代器不再有.next属性,只有next函数,所以只能使用__next__私有属性,而非可以被外部调用的.next公有属性 """
+   """ 本例中迭代返回值是self.a计数器 """
    def __next__(self):
       if self.a <self.x * self.y:
          x = self.a
@@ -302,7 +336,7 @@ class iteration001:
       else:
          raise StopIteration
 
-cls001=iteration001(2,3)
+cls001=iteration001(1,5)
 ite008_1=iter(cls001)
 # print(next(ite008_1),next(ite008_1),sep=",")
 # for x in ite008_1:
